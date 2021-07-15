49a317d331bad6f9de650f7ee26990cd05cb534c
Nerpinary <nerpinary5693@gmail.com>
2021-07-15 23:24:22 +0800

    test

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,11 @@ import FormValidator from "../components/FormValidator.js"
 import { config } from "../utils/config.js"
 import { entryForm, inputRub, inputValute, select } from "../utils/constants.js"
 
-
+//Включаем валидацию поля почты
 const validateEmail = new FormValidator(config, entryForm);
 validateEmail.enableValidation();
 
+//Создаем пустой объект для последующего добавления списка валют
 let rates = {}
 
 //Получаем объект с курсами валют
@@ -15,17 +16,18 @@ async function getCurrencies() {
     const result = await data;
 
     rates = result.Valute
+    //Преобразуем ключи объектов в массив
     let valutes = Object.keys(result.Valute)
     let values = Object.values(result.Valute)
-    
+    //Добавляем новые опции для селектора из полученных от сервера
     for (let i = 0; i < valutes.length; i++) {
         let newOption = new Option(valutes[i] + ` - ${values[i].Name}`)
         select.append(newOption)
     }
-
+    //Добавляем слушатели для инпутов
     inputRub.oninput = converteValute;
     select.oninput = converteValute;
-    
+    //Вынесли конвертацию в отдельную функцию
     function converteValute() {
         inputValute.value = (parseFloat(inputRub.value) / rates[select.value.substr(0, 3)].Value).toFixed(2)
     }
